a8839e18857bea4a47b4a42e955e54480ff30395
Harshit Chadha <harshitchadha@Harshits-MacBook-Pro.local>
2024-07-05 16:52:07 +0530

    header done

--- app/(tabs)/main.tsx
+++ app/(tabs)/main.tsx
@@ -2,12 +2,12 @@ import { View, Text } from "react-native";
 import React from "react";
 import { Link } from "expo-router";
 import { SafeAreaView } from "react-native-safe-area-context";
+import Header from "@/components/Home/Header";
 
 const main = () => {
   return (
     <SafeAreaView>
-      <Text>maijhcjbdjbjbbdfjbjfbn</Text>
-      <Link href={'/(products)'}>j</Link>
+      <Header />
     </SafeAreaView>
   );
 };
