3ac68a63d1b01d55ecdf29ba31168f6048e6d72c
Harshit Chadha <harshitchadha@Harshits-MacBook-Pro.local>
2024-07-19 14:34:38 +0530

    product fetching for category

--- components/Home/CategoriesSection.tsx
+++ components/Home/CategoriesSection.tsx
@@ -2,11 +2,35 @@ import { View, Text } from "react-native";
 import React, { useEffect, useState } from "react";
 import CategoryToggle from "./CategoryToggle";
 import { getCategories } from "@/context/CategoriesProvider";
+import { query, collection, where, limit, getDocs } from "firebase/firestore";
+import { db } from "@/firebaseConfig";
 
 const CategoriesSection = () => {
   const [activeTab, setActiveTab] = useState(0);
   const { categories } = getCategories();
   console.log(categories);
+  const [productList, setProductList] = useState(null);
+
+  const getProducts = async () => {
+    const q = query(
+      collection(db, "Products"),
+      where("category", "==", activeTab)
+    );
+
+    const snapShot = await getDocs(q);
+    const list = snapShot.docs.map((doc) => ({
+      id: doc.id,
+      title: doc.data().name,
+      images: doc.data().images,
+      price: doc.data().price,
+    }));
+
+    console.log(list);
+  };
+
+  useEffect(() => {
+    getProducts();
+  }, [activeTab]);
   return (
     <View className=" w-[90%] m-auto my-5 px-2 rounded-lg flex-col">
       <Text className="text-xl font-bold">Shop By Categories</Text>
@@ -15,6 +39,7 @@ const CategoriesSection = () => {
         activeTab={activeTab}
         setActiveTab={setActiveTab}
       />
+      
     </View>
   );
 };
