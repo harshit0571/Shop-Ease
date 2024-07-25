f38af47f4096b9df8bdae6783dd988f7c199a52a
Harshit Chadha <harshitchadha@Harshits-MacBook-Pro.local>
2024-07-25 19:42:12 +0530

    footer added

--- components/Home/ProductCardContainer.tsx
+++ components/Home/ProductCardContainer.tsx
@@ -1,9 +1,9 @@
-import { View, Text } from "react-native";
+import { View, Text, ActivityIndicator } from "react-native";
 import React from "react";
 import ProductCard from "../common/ProductCard";
 
 interface propsInterface {
-  productList: any;
+  productList: productInterface[];
 }
 interface productInterface {
   title: string;
@@ -12,19 +12,24 @@ interface productInterface {
   id: any;
 }
 const ProductCardContainer = ({ productList }: propsInterface) => {
-  console.log(productList);
+  console.log(productList?.length);
   return (
     <View className="flex flex-row flex-wrap gap-3 mt-5">
-
-      {productList?.map((product: productInterface) => (
-        <ProductCard
-          key={product.id}
-          title={product.title}
-          price={product.price}
-          images={product.images}
-          id={product.id}
-        />
-      ))}
+      {productList?.length > 0 ? (
+        productList?.map((product: productInterface) => (
+          <ProductCard
+            key={product.id}
+            title={product.title}
+            price={product.price}
+            images={product.images}
+            id={product.id}
+          />
+        ))
+      ) : (
+        <View className="flex justify-center items-center  w-full">
+          <ActivityIndicator className="text-red-300 m-auto" color={"red"} size={"large"}/>
+        </View>
+      )}
     </View>
   );
 };
